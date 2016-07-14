9b7ae3c7f22bec5e05877a187ac19e62860f49b2
marczenn <marcusareulius@gmail.com>
2016-07-13 21:19:31 -0600

    abstracted homepage nav bar into seperate Component

--- Components/Home/home.jsx
+++ Components/Home/home.jsx
@@ -1,11 +1,14 @@
 import React, {Component} from 'react'
 
 // TODO: strip nav out into seperate Component
-// TODO: think of a better slogan to place on home page
 // TODO: plan out the rest of the site
 // TODO: give credit to original designer somewhere.
 // TODO: make mobile responsive
 // TODO: 'upgrading UI' spinner before client display work page
+// TODO: incorporate google analytics to track site hits.
+
+// Components 
+import HomeNav from './HomeNav.jsx';
 
 // Stylesheets
 require('../../public/styles/home.scss');
@@ -18,14 +21,8 @@ class Homepage extends Component {
         <div className="overlay"></div>
         <div className="scanline"></div>
         <div className="wrapper">
-          <nav className="site clear home-nav">
-            <ul className="home-nav-list">
-              <li><a href="#" title="">Companies</a></li>
-              <li><a href="#" title="">Works</a></li>
-              <li><a href="#" title="">About</a></li>
-              <li><a href="#" title="">Contact</a></li>
-            </ul>
-          </nav>
+
+          <HomeNav />
 
           <div className="home-content clearfix">
             <header className="site clearfix home-header">
@@ -34,43 +31,25 @@ class Homepage extends Component {
               </div>
               <div className="col two">
                 <h1><br /> <b>M</b>erits <b>A</b>nalysis <b>R</b>eal-Time <b>C</b>omputer (M.A.R.C)</h1>
-                <p className="home-copy">System Administrator (SYSADM) - Marc Zenn</p>
                 <p>---------------------------------------------------</p>
-                <p className="home-copy">Fanatic of all things tech, innovative & socially-conscience missions.</p>
-                <p>Version 1.0.0 - Server 0089</p>
+                <p className="slogan">Fanatic of all things tech, innovative & socially-conscience missions.</p>
+                <p className="home-copy">System Administrator (SYSADM) - Marc Zenn</p>
+                <p className="home-copy">Version 1.0.0 - Server 0089</p>
               </div>
             </header>
 
             <div className="description-content">
               <h2>Administrator Biography &amp; Official Under-Taking  (A.B.O.U.T)</h2>
               <div>
-                <p className="home-copy">I'm a deep thinker and an overtly anaylitcal person that, despite certain challenges, has managed to find a calling in life and attained the means to pursue it. These days I spend my time living and working in beautiful Boulder, Colorado enjoying my post-CU, (University of Colorado boulder), days as a web engineer.</p>
-                <p className="home-copy">As a kid I felt an incredible pressure to succeed and support my family. To lift all tides, so to speak. Since then I've realized no one succeeds by the combined efforts of themselves alone and that very few succeed without risk or the pursuit of overlooked value/opportunity.</p>
-                <p className="home-copy">But true success also depends on how you define it. For me, success means the ability to gather disparate groups of people for the simple idea of social progress. There is a myriad of problems facing the world today. If I could have even a small role in solving at least one of those problems, I'd consider myself successful. </p>
+                <p className="home-copy">I'm a deep thinker and an overtly anaylitcal person that, despite a number of circumstantial challenges, has managed to find a calling in life and is working hard to pursue it. These days I spend my time living and working in beautiful Boulder, Colorado enjoying my University of Colorado days as a web engineer and startup enthusiast.</p>
+                <p className="home-copy">As a kid I felt an incredible pressure to succeed and support my family while at the same time embarking on work that would one day have an impact. Since then I've realized no one succeeds by the combined efforts of themselves alone and that very few succeed without risk or the pursuit of overlooked value/opportunity.</p>
+                <p className="home-copy">But true success also depends on how you define it. For me, success is the ability to gather groups of people under the simple idea that when united, social progress is attainable. There is a myriad of problems facing the world today. If I could have even a small role in solving at least one of those problems or in altering the course of mankind, I'd consider myself successful.</p>
               </div>
             </div>
 
-            <div className="work-content">
-              <h2>Web Orientated Recently Kompleted Singletons (W.O.R.K.S)</h2>
-              <div>
-                <ul className="works-list">
-                  <li><a href="http://www.kickfurther.com"><img src="./public/images/kf_logo_large.png" /></a></li>
-                  <li><a href="http://www.kickfurther.com"><img src="./public/images/" /></a></li>
-                  <li><a href="http://www.kickfurther.com"><img src="" /></a></li>
-                </ul>
-              </div>
-            </div>
-
-            <div className="home-social-bar">
-              <ul>
-                <li><a href="https://github.com/MarcZenn">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/marcosmartinez2">LinkedIn</a></li>
-                <li><a href="">Google+</a></li>
-              </ul>
-            </div>
 
             <div className="contact-content">
-              <h3>Contact</h3>
+              <h2>Contact</h2>
               <p>Fill out the fields below and press the SUBMIT button. The system administrator (SYSADM) will respond to your query after an appropriate amount of quiet contemplation. Thank you for contacting the System Administrator's Office.</p><br />
               <form className="home-contact-form">
                 <p>Welcome to the System Administrator Integrated Message System (SAIMS).</p>
@@ -83,6 +62,14 @@ class Homepage extends Component {
               </form>
             </div>
 
+            <div className="home-social-bar">
+              <ul>
+                <li><a href="https://github.com/MarcZenn">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/marcosmartinez2">LinkedIn</a></li>
+                <li><a href="">Google+</a></li>
+              </ul>
+            </div>
+
           </div>
         </div>
       </section>
@@ -91,7 +78,7 @@ class Homepage extends Component {
 }
 
 Homepage.propTypes = {
- // none needed for home page
+ // propTypes
 }
 
 
