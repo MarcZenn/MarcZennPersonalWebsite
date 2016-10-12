b1486aee93cf1fe1b6beca3b9a4f56085ae719db
marczenn <marcusareulius@gmail.com>
2016-10-12 14:30:12 -0600

    production path fix attempt

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ const compiler = webpack(config);
 
 
 // Setup server paths
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/../')); // Serve from root directory
 app.use(webpackMiddleware(compiler));
 app.use(webpackHotMiddleware(compiler));
 
 app.get('*', function response(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(8080);
