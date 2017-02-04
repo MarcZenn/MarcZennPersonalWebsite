cb23b379e067deb55324b9efd6f029595f31269b
marczenn <marcusareulius@gmail.com>
2017-02-03 18:27:34 -0700

    image compression webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,13 @@ module.exports = {
       loader: 'url-loader'
       },
       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      },
+      {
         test: /\.css$/, loader: 'style!css'
       }
     ]
