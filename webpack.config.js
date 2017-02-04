7a6d2425ca68ec5dfd2c821728ae00269cbe33c0
marczenn <marcusareulius@gmail.com>
2017-02-03 17:57:15 -0700

    try all the loaders

--- webpack.config.js
+++ webpack.config.js
@@ -41,14 +41,12 @@ module.exports = {
         loader: 'url-loader?limit=100000'
       },
       {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
-      },
-      {
         test: /\.css$/, loader: 'style!css'
-      }
+      },
+      {test: /\.woff(2)?(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" }
     ]
   },
   plugins: [
