ed4ea9c9bff676bf6436524dcaf49234c3601a4b
marczenn <marcusareulius@gmail.com>
2017-03-14 11:51:15 -0600

    link to resume page

--- resources/Components/Static/About/About.jsx
+++ resources/Components/Static/About/About.jsx
@@ -14,14 +14,15 @@ class About extends Component {
       <section>
         <Navigation />
         <div className="about-content">
-          <p className="about-copy">When you start at the bottom of a well you have to climb the usual ladders several rungs at a time. Nothing comes easy, there's no such thing as a safe outcome, plans always go awry and if you're not careful, you can easily become the victim of others' expectations. A victim of "you can't do that" or "I doubt you'll get there with your background" or "it's just too expensive". These perspectives are nothing but obstacles. Obstacles that give way to challenges, and challenges are meant to be overcome.</p>
-          <p className="about-copy">It's my firm belief that no one succeeds by the combined efforts of themselves alone and that very few succeed without risk or the pursuit of overlooked value/opportunity. That success depends on how you define it as much as how you attain it and that success alone is not enough if it only benefits you, maybe even a select few.</p>
+          <p className="about-copy">When you start at the bottom of a well you have to rise up several leaps at a time. Nothing comes easy, there's no such thing as a safe outcome, plans often go wrong and if you're not careful, you can easily become the victim of others' expectations. Expectations such as, "you can't do that" or "I doubt you'll get there with your background" or "it's too expensive". In reality these things are nothing but obstacles. Obstacles that give way to challenges, and challenges are meant to be overcome. </p>
+          <p className="about-copy">The pursuit of overcoming problems and challenges is what I live for. But no one succeeds by the combined efforts of themselves alone. My success depends on how I define it as much as how I attain it and it's not enough if it only benefits me.</p>
           <h1>Work</h1>
           <p className="about-copy">
-            Currently I'm experienced in object-oriented programming, agile, testing, debugging, refactoring & evaluating the full-stack. Hoping to dive into Ethereum Decentralization Platform, Augmented/Virtual Reality, Machine-Learning, Social E-commerce, AI, Statically Typed Languages. For the time being I work for a successful startup as a Junior Developer and 9th team hire.
-          </p>
+            Currently I'm experienced in object-oriented programming, agile, testing, debugging, refactoring & evaluating the full-stack. I work for a successful startup as an intermediate web engineer and 9th team hire.
+          </p><br/>
+          <p>For a list of known technlogies you can check out my <a href="http://www.resume.com/share/p4uzoufBAJ5ibsBf6">resume page.</a></p>
           <h1>Life</h1>
-          <p className="about-copy">These days I spend my time living and working in beautiful Boulder, Colorado enjoying my days as a web engineer and startup enthusiast. The rest of the time I'm pursuing something I personally feel passionate about. I think the one thing I'd say my soul couldn't live without would be music...if only I had any musical talent whatsoever. Regardless, I'm good have to settle for the occasional hike or road trip and a good beer or two once in a while.</p>
+          <p className="about-copy">I live and work in beautiful Boulder, Colorado enjoying my days as a tinkerer and startup enthusiast. The rest of the time I'm pursuing something I personally feel passionate about. If I had any musical talent whatsoever there would be a good chance I'd have a couple of hits out by now.. Regardless, I'm good with the occasional hike, road trip and a beer or two now and then.</p>
         </div>
         <Footer />
       </section>
